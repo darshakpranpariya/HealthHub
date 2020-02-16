3f475666dd6ecc1350e71b177cfc23314438cb73
Darshak <darshak.ranpariya@gmail.com>
2020-02-16 22:46:50 +0530

    commit

--- HealthHub/lib/text_reco.dart
+++ HealthHub/lib/text_reco.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
-
 
 class Text_Reco extends StatefulWidget {
   @override
@@ -14,81 +12,82 @@ class Text_Reco extends StatefulWidget {
 class _Text_RecoState extends State<Text_Reco> {
   File pickedImage;
   bool isImageLoaded = false;
-
-  Future<bool> checkAndRequestCameraPermissions() async {
-  PermissionStatus permission =
-      await PermissionHandler().checkPermissionStatus(PermissionGroup.camera);
-  if (permission != PermissionStatus.granted) {
-    Map<PermissionGroup, PermissionStatus> permissions =
-        await PermissionHandler().requestPermissions([PermissionGroup.camera]);
-    return permissions[PermissionGroup.camera] == PermissionStatus.granted;
-  } else {
-    return true;
-  }
-}
-
+  String s="";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        FloatingActionButton(
-          heroTag: "btn1",
-          child: Icon(Icons.photo),
-          onPressed: pickImage,
-          backgroundColor: Colors.pink,
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.20,
-        ),
-        isImageLoaded
-            ? Center(
-                child: Container(
-                    height: 200.0,
-                    width: 200.0,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: FileImage(pickedImage), fit: BoxFit.cover))),
-              )
-            : Container(),
-        FloatingActionButton(
-          heroTag: "btn2",
-          child: Icon(Icons.arrow_forward_ios),
-          onPressed: readText,
-          backgroundColor: Colors.pink,
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.20,
-        ),
-      ],
-    ));
+        body: ListView(children: <Widget>[
+      Column(
+        children: <Widget>[
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.03,
+          ),
+          isImageLoaded
+              ? Center(
+                  child: Container(
+                      height: 300.0,
+                      width: 300.0,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: FileImage(pickedImage),
+                              fit: BoxFit.cover))),
+                )
+              : Container(
+                  height: 200.0,
+                  width: 200.0,
+                ),
+          SizedBox(height: 15.0),
+          Text(s),
+          Padding(
+            padding: const EdgeInsets.only(top: 300),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                FloatingActionButton(
+                  heroTag: "btn1",
+                  child: Icon(Icons.photo),
+                  onPressed: pickImage,
+                  backgroundColor: Colors.pink,
+                ),
+                FloatingActionButton(
+                  heroTag: "btn2",
+                  child: Icon(Icons.arrow_forward_ios),
+                  onPressed: readText,
+                  backgroundColor: Colors.pink,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ]));
   }
 
   Future pickImage() async {
-    
-    if (true) {
     var tempStore = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (tempStore == null) {
+      Navigator.pop(context);
+    }
     setState(() {
       pickedImage = tempStore;
       isImageLoaded = true;
-    });}
-    else
-      print("not provide");
+    });
   }
+
+  VisionText rt;
 
   Future readText() async {
     FirebaseVisionImage ourImage = FirebaseVisionImage.fromFile(pickedImage);
     TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
-    VisionText readText = await recognizeText.processImage(ourImage);
-
-    for (TextBlock block in readText.blocks) {
-      for (TextLine line in block.lines) {
-        for (TextElement word in line.elements) {
-          print(word.text);
+    rt = await recognizeText.processImage(ourImage);
+    setState(() {
+      for (TextBlock block in rt.blocks) {
+        for (TextLine line in block.lines) {
+          for (TextElement word in line.elements) {
+            s += " " + word.text;
+          }
         }
       }
-    }
+    });
   }
-
-  
 }
